26da9dc451ddbeb8f957e9faad1f85b891cfe838
vladimir.kartyaev <vladimir.kartyaev@gmail.com>
2021-11-28 17:12:27 +0400

    hw2

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,43 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	length := len(s)
+	if length == 0 {
+		return "", nil
+	}
+	if unicode.IsDigit(rune(s[0])) {
+		return "", ErrInvalidString
+	}
+	asIsNext := false
+	result := strings.Builder{}
+	for i := 0; i < length; i++ {
+		if s[i] == '\\' && !asIsNext {
+			asIsNext = true
+			continue
+		}
+		if asIsNext && !unicode.IsDigit(rune(s[i])) && s[i] != '\\' {
+			return "", ErrInvalidString
+		}
+		if i+1 < length && unicode.IsDigit(rune(s[i+1])) {
+			if i+2 < length && unicode.IsDigit(rune(s[i+2])) {
+				return "", ErrInvalidString
+			}
+			counter, _ := strconv.Atoi(string(s[i+1]))
+			result.WriteString(strings.Repeat(string(s[i]), counter))
+			i++
+			asIsNext = false
+		} else {
+			result.WriteRune(rune(s[i]))
+			asIsNext = false
+		}
+	}
+	return result.String(), nil
 }
